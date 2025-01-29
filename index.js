07ed525fda04c091fc67ae4e9cd088d694608c30
Ayush Anand <ayushanandhd@gmail.com>
2025-01-29 22:29:02 +0530

    update project

--- index.js
+++ index.js
@@ -51,9 +51,7 @@ io.on('connection', (socket) => {
   });
 });
 
-// const PORT = process.env.PORT || 3000;
-// server.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
-module.exports=app;
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
