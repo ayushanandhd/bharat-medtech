ab6d7e53b19d2548aef7798c067d9fd27b488766
Ayush Anand <ayushanandhd@gmail.com>
2025-01-29 21:33:34 +0530

    update project for vercel

--- app.js
+++ app.js
@@ -51,4 +51,7 @@ io.on('connection', (socket) => {
   });
 });
 
-module.exports = app;
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
